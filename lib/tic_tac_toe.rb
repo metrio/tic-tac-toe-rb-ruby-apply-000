a1e96e337ddcadcf43f9b9e41577557158d4f075
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-25 20:01:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,6 +24,6 @@ def input_to_index(user_input)
   index = user_input.to_i - 1    
 end
 
-def move(board, index, player = "X")  #player is defaulted to X
+def move(board, index, )  #player is defaulted to X
 board[index]= player
 end
