c8d55811f3a17dae246ad36c7e75ad243a107291
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-25 20:15:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,7 +25,16 @@ def input_to_index(user_input)
 end
 
 def move(board, index, player) 
-board[index]= player
+  board[index]= player
 end
 
-def position_taken?
+def position_taken?(board, index)
+
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    false
+  
+  elsif
+    (board[index] == "X" || board[index] == "O")
+     true
+  end  
+end
